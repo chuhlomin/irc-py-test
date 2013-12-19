b43e2aebd7391024faae0fb030e8d736ef94471f
Константин Чухломин <k.chukhlomin@gmail.com>
2013-12-19 22:27:47 +0400

    Bots asks JIRA about issue summary

--- bot.py
+++ bot.py
@@ -1,23 +1,30 @@
 from socket import *
+from jira.client import JIRA
 
 HOST = '137.116.220.12'
 PORT = 6667
 
-NICK_NAME = "irc-py-test"
+NICK_NAME = "bot-py"
 USER_NAME = "Test Bot"
 REAL_NAME = "Test Bot (really)"
 HOST_NAME = ""
 SERVER_NAME = "DengiOnlineNet"
-CHANNEL_NAME = "#cp"
+
+options = {
+    'server': 'https://jira.dengionline.com',
+}
+
+basic_auth = ('chuhlomin', '')
+
+jira = JIRA(options, basic_auth)
 
 
 class Irc_wrapper:
-    def __init__(self, host, port, nick_name, channel_name, user_name, host_name, server_name, real_name):
+    def __init__(self, host, port, nick_name, user_name, host_name, server_name, real_name):
         self.data = ''
         self.host = host
         self.port = port
 
-        self.channel_name = channel_name
         self.irc = socket(AF_INET, SOCK_STREAM)
 
         self.init_irc()
@@ -42,8 +49,8 @@ class Irc_wrapper:
     def private_message(self, message):
         self.command("PRIVMSG %s\r\n" % message)
 
-    def join(self):
-        self.command("JOIN %s\r\n" % self.channel_name)
+    def join(self, channel_name):
+        self.command("JOIN #%s\r\n" % channel_name)
 
     def nick(self, nick_name):
         self.command("NICK %s\r\n" % nick_name)
@@ -55,7 +62,7 @@ class Irc_wrapper:
         self.command("PONG %s\r\n" % data)
 
 
-irc_client = Irc_wrapper(HOST, PORT, NICK_NAME, CHANNEL_NAME, USER_NAME, HOST_NAME, SERVER_NAME, REAL_NAME)
+irc_client = Irc_wrapper(HOST, PORT, NICK_NAME, USER_NAME, HOST_NAME, SERVER_NAME, REAL_NAME)
 
 while True:
 
@@ -65,8 +72,9 @@ while True:
     if irc_client.find_in_data('PING'):
         irc_client.pong(irc_client.data.split()[1])
 
-    elif irc_client.find_in_data('Current Global Users'):
-        irc_client.join()
+    elif irc_client.find_in_data('INVITE'):
+        channel = irc_client.data.split('#')[-1]
+        irc_client.join(channel)
 
     elif irc_client.find_in_data('PRIVMSG'):
 
@@ -85,5 +93,22 @@ while True:
 
             message = destination + " :" + nick + ": " + solution
             irc_client.private_message(message)
+
+        elif message.lower().find('%jira-task-title') == 0:
+
+            nick = irc_client.data.split('!')[0].replace(':', '')
+            destination = ''.join(irc_client.data.split(':')[:2]).split(' ')[-2]
+            task = message.split(' ')[1].rstrip('\r\n')
+
+            try:
+                issue = jira.issue(task)
+                task_title = issue.fields.summary.encode('utf-8')
+            except:
+                task_title = 'Some error...'
+
+            message = destination + " :" + nick + ": " + task_title
+
+            irc_client.private_message(message)
+
 
 irc_client.irc.close()
